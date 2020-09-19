3641f58cd613d1603b7754c466e7b22195150d96
James Kim <pgcjameskim@gmail.com>
2020-09-18 20:42:50 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import React, {useState,useEffect} from 'react';
 import axios from 'axios'
 import './App.css';
-
 import Layout from './Layout/Layout'
 import SearchWeather from './SearchWeather/SearchWeather'
+import Spinner from './Spinner/Spinner'
 
 const App = () => {
   const [input, onInput] = useState('')
   const [submitData, onSubmitData] = useState('') 
   const [isDay, setIsDay] = useState(true) 
+  const [loading, setLoading] = useState('initial')
 
   useEffect(() => {
     if (submitData.is_day === 'no') {
@@ -23,10 +24,19 @@ const App = () => {
   }
 
   const onSubmitHandler = async (event) => {
+      setLoading('true')
       event.preventDefault()
       const response  = await axios.get(`/weather?address=${input}`)
       const data = response.data
       onSubmitData(data)
+      setLoading('false')
+  }
+
+  let jsx
+  if (loading === 'true') {
+    jsx = <Spinner />
+  } else {
+    jsx = <SearchWeather submitData={submitData}input= {input} inputHandler={onInputHandler} request={onSubmitHandler}/>
   }
 
     let dayOrNight 
@@ -36,9 +46,10 @@ const App = () => {
 
   return (
         <Layout isDay={isDay} background={dayOrNight}>
-            <SearchWeather submitData={submitData}input= {input} inputHandler={onInputHandler} request={onSubmitHandler}/>
+            {jsx}
         </Layout>
   );
 }
 
 export default App;
+
